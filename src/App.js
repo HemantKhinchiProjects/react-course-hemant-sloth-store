71a4282cc661e71c267b924dff5aaeb0dcf95dd3
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-21 18:15:09 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -21,6 +21,12 @@ export default function App() {
           <Route exact path="/about" element={<About />} />
           <Route exact path="/cart" element={<Cart />} />
           <Route exact path="/products" element={<Products />} />
+          <PrivateRoute
+            path="/private"
+            component={Private}
+            element={<Checkout />}
+          />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
