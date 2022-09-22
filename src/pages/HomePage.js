8af573b8502cdfb263db6062cc5bccad255faf50
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-22 16:15:16 +0530

    1 file modified, 1 added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Hero from '../components/Hero';
 import FeaturedProducts from '../components/FeaturedProducts';
+import Services from '../components/Services';
+import Services from '../components/Services';
 const HomePage = () => {
   return (
     <main>
+      <Hero />
       <FeaturedProducts />
+      <Services />
     </main>
   );
 };
