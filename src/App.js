bd450696df7986f74074d89a525a0d69381d9ace
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-27 17:07:35 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
           <Route exact path="/about" element={<About />} />
           <Route exact path="/cart" element={<Cart />} />
           <Route exact path="/products" element={<Products />} />
+
           {/* <PrivateRoute
             path="/private"
             component={Private}
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,33 @@
-import React from 'react'
-const Error = () => {
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+const ErrorPage = () => {
   return (
-    <div className='section section-center text-center'>
-      <h2>there was an error...</h2>
-    </div>
-  )
-}
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
+};
 
-export default Error
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`;
+
+export default ErrorPage;
