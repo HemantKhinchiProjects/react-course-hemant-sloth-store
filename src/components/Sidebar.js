ff89da372e489c54ffec280e2af7da3ab4c6d6f8
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-27 14:21:40 +0530

    2 files modified

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { useProductsContext } from '../context/products_context';
 import { FaTimes } from 'react-icons/fa';
 import { links } from '../utils/constants';
 import styled from 'styled-components';
-import CartButtons from './CartButtons/CartButtons';
+//import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useProductsContext();
@@ -31,7 +31,7 @@ const Sidebar = () => {
             );
           })}
         </ul>
-        <CartButtons />
+        {/* <CartButtons /> */}
       </aside>
     </SidebarContainer>
   );
