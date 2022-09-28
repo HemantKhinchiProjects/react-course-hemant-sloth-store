cc1ccbca87600859b82ee14325843aa3f4c8d5a7
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-28 07:44:22 +0530

    1 file modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,14 +14,14 @@ const Contact = () => {
           </p>
           <form
             className="contact-form"
-            action="your form spree id"
+            action="https://formspree.io/f/xzbwrqkp"
             method="POST"
           >
             <input
               type="email"
               className="form-input"
               placeholder="enter email"
-              name="_replyto"
+              name="email"
             />
             <button type="submit" className="submit-btn">
               subscribe
