767d6479de03016b62e2a13fa95b6598d74eed48
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-28 08:20:58 +0530

    1 file modified

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,21 +1,21 @@
 import React from 'react';
-//import { useProductsContext } from '../context/products_context';
+import { useProductsContext } from '../context/products_context';
 import { Link } from 'react-router-dom';
 import Error from './Error';
 import Loading from './Loading';
 import styled from 'styled-components';
 const FeaturedProducts = () => {
-  // const {
-  //   products_loading: loading,
-  //   products_error: error,
-  //   featured_products: featured,
-  // } = useProductsContext();
-  // if (loading) {
-  //   return <Loading />;
-  // }
-  // if (error) {
-  //   return <Error />;
-  // }
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
   return (
     <Wrapper className="section">
       <div className="title">
